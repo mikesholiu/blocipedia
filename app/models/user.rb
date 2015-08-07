fa36a29fad948c155b9e3700a52909146bb9a23c
Mike Liu <mikesholiu@gmail.com>
2015-08-06 19:44:41 -0700

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
+      user.image = auth.info.image
     end
   end 
 
