acf16e8cc060f1bb56a53bb77bfaa56920819be6
Mike Liu <mikesholiu@gmail.com>
2015-08-09 00:16:24 -0700

    created wiki model and controller, starting crud

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806061245) do
+ActiveRecord::Schema.define(version: 20150809054427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,4 +41,16 @@ ActiveRecord::Schema.define(version: 20150806061245) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "wikis", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "private"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id", using: :btree
+
+  add_foreign_key "wikis", "users"
 end
