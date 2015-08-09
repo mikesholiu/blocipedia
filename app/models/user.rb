4f52fa1b87aaeccf0cea833c7ebfb808229a4f92
Mike Liu <mikesholiu@gmail.com>
2015-08-09 10:27:59 -0700

    adding twitter profile picture to edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
 
   def self.from_omniauth(auth)
+    p auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.image = auth.info.image
+      user.name = auth.info.name
     end
   end 
 
